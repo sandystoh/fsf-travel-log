13119d4d148101116cb7ac55a80ed5d9ae55ec21
sandystoh <sandytoh@u.nus.edu>
2019-12-10 14:07:37 +0800

    first draft detail

--- travel-app-client/src/app/components/place-detail.component.ts
+++ travel-app-client/src/app/components/place-detail.component.ts
@@ -11,9 +11,30 @@ import { Place } from '../models';
 export class PlaceDetailComponent implements OnInit {
 
   id: number;
-  place: Place = { "id": 86, "journey_id": 2, "journey_order": 2, "type": "BEEN", "title": "THe United Federation of Planets Headquarters", 
-  "owner": "sandystoh", "date": "2017-01-05T16:00:00.000Z", "lat": -37.8109, "lng": 175.7765, "country": "NZ", "rating": 3, "image_url": "sandystoh/4d1e3f5745ae8cb4d83040dd85fc6fd3", "description": "Lovely Place 4!", "private_notes": "Got Lost in the Hobbit Holes!", "last_updated": "2019-11-04T16:00:00.000Z", "active": 1, "journey_title": "New Zealand", "journey_count": 3 }
-
+  place: Place = {
+    id: 2,
+    journey_id: 1,
+    journey_order: 2,
+    type: "BEEN",
+    title: "Bryce Canyon National Park",
+    owner: "sandystoh",
+    date: "2016-07-05T16:00:00.000Z",
+    lat: 37.593,
+    lng: -112.1871,
+    country: "US",
+    rating: 4,
+    image_url: null,
+    description: "Lovely Place 2!",
+    private_notes: "Got Lost in the Desert!",
+    last_updated: "2019-11-04T16:00:00.000Z",
+    active: 1,
+    journey_title: "United States and Canada and Mexico and Cayman Islands",
+    journey_count: 4,
+    next_id: 80,
+    prev_id: 1,
+    country_name: "United States of America"
+    };
+  
   constructor(private router: Router, private route: ActivatedRoute,
               private travelSvc: TravelService) { }
 
